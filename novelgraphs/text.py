2f6a4864c6bfddb85fed827daeac921cb7a35e27
Pavel Novichkov <novi4koff@gmail.com>
2016-03-14 00:46:43 +0300

    Add tokenizer backend parameter to Text class
    Closes #2

--- novelgraphs/text.py
+++ novelgraphs/text.py
@@ -1,9 +1,14 @@
 import pickle
+from novelgraphs.annotators import Tokenizer
 
 
 class Text(object):
-    def __init__(self, raw_text):
+    def __init__(self, raw_text, tokenizer='corenlp',
+                 corenlp_path="./stanford-corenlp-full-2015-12-09/"):
         self._raw_text = raw_text
+        if tokenizer is not None:
+            Tokenizer(backend=tokenizer,
+                      corenlp_path=corenlp_path).annotate(self)
 
     def to_pickle(self, filename):
         with open(filename, "wb") as out_file:
