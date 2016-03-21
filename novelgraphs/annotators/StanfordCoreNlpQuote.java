e8892977568f0aef813a9f34eff642e1a3ea433b
Pavel Novichkov <novi4koff@gmail.com>
2016-03-21 23:45:34 +0300

    Add single_quotes and max_length parameters
    Closes #3

--- novelgraphs/annotators/StanfordCoreNlpQuote.java
+++ novelgraphs/annotators/StanfordCoreNlpQuote.java
@@ -9,28 +9,14 @@ import edu.stanford.nlp.util.*;
 public class StanfordCoreNlpQuote {
 
   public static void main(String[] args) throws IOException {
-    PrintWriter out;
-    if (args.length > 1) {
-      out = new PrintWriter(args[1]);
-    } else {
-      out = new PrintWriter(System.out);
-    }
-
     Properties props = new Properties();
+    props = StringUtils.argsToProperties(args);
     props.put("annotators", "tokenize, ssplit, quote");
-    props.put("quote.singleQuotes", "true");
-    // props.put("quote.asciiQuotes", "true");
-    props.put("tokenize.whitespace", "true");
-    props.put("ssplit.eolonly", "true");
 
+    String filename = props.getProperty("file");
+    props.remove("file");
+    Annotation annotation = new Annotation(IOUtils.slurpFileNoExceptions(filename));
     StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
-
-    Annotation annotation;
-    if (args.length > 0) {
-      annotation = new Annotation(IOUtils.slurpFileNoExceptions(args[0]));
-    } else {
-      annotation = new Annotation("Kosgi Santosh sent an email to Stanford University. He did not get a reply.");
-    }
 
     pipeline.annotate(annotation);
 
--- novelgraphs/annotators/quote.py
+++ novelgraphs/annotators/quote.py
@@ -9,25 +9,35 @@ def _table_to_list(splitted_sent):
         tokens_file.write('\n'.join([' '.join(s) for s in splitted_sent]))
 
 
-def _corenlp_quote_annotate(corenlp_path):
+def _corenlp_quote_annotate(corenlp_path, single_quotes, max_length):
+    args_single_quotes = ['-quote.singleQuotes'] if single_quotes else []
+    args_max_length = (['-quote.maxLength', str(max_length)]
+                       if max_length is not None else [])
     path = os.path.dirname(os.path.realpath(__file__))
     with open('quotes.txt', 'w') as quotes_file:
         subprocess.call(['java',
                          '-Xmx2g',
                          '-cp', path + ":" + corenlp_path + "*",
                          'StanfordCoreNlpQuote',
-                         'tokens.txt'],
+                         '-tokenize.whitespace',
+                         '-ssplit.eolonly',
+                         '-file', 'tokens.txt'] +
+                        args_max_length + args_single_quotes,
                         stdout=quotes_file)
 
 
 class Quote(Annotator):
-    def __init__(self, corenlp_path="./stanford-corenlp-full-2015-12-09/"):
+    def __init__(self, corenlp_path="./stanford-corenlp-full-2015-12-09/",
+                 single_quotes=False, max_length=None):
         Annotator.__init__(self)
         self._corenlp_path = corenlp_path
+        self._single_quotes = single_quotes
+        self._max_length = max_length
 
     def annotate(self, text):
         _table_to_list(text.tokens)
-        _corenlp_quote_annotate(self._corenlp_path)
+        _corenlp_quote_annotate(self._corenlp_path, self._single_quotes,
+                                self._max_length)
         quotation_ids = pd.read_csv(
             'quotes.txt', sep='\t', quoting=3,
             header=None, usecols=[1], squeeze=True)
